0cfbbf6f320c29a1872ef7bc6ed9217225e46edf
Matthew Fuller <matt.j.fuller@outlook.com>
2020-07-19 16:09:09 +0100

    update - title delays

--- script.js
+++ script.js
@@ -9,31 +9,94 @@ const name5 = document.querySelector(".hover-ef5");
 const container = document.querySelector(".main-container");
 const body = document.querySelector("body");
 
+setTimeout(() => {
+
+    name3.classList.add("name-on");
+    setTimeout(() => {
+        name3.classList.add("animate__fadeOut")
+    }, 1000);
+    setTimeout(() => {
+        name3.classList.remove("name-on")
+        name3.classList.remove("animate__fadeOut")
+        name3.classList.add("lock3", "animate__fadeIn")
+        }, 3000)
+
+    setTimeout(() => {
+        name4.classList.add("name-on");
+        setTimeout(() => {
+            name4.classList.add("animate__fadeOut")
+        }, 1000);
+        setTimeout(() => {
+            name4.classList.remove("name-on")
+            name4.classList.remove("animate__fadeOut")
+            name4.classList.add("lock4", "animate__fadeIn")
+            }, 3000)
+        }, 2000)
+
     setTimeout(() => {
         name.classList.add("name-on");
-    setTimeout(() => {
-        name.classList.add("move")
-    }, 3000)
-    name2.classList.add("name-on")
-    setTimeout(function() {
-        name2.classList.add("move2")
-    }, 3000)
+        setTimeout(() => {
+            name.classList.add("animate__fadeOut")
+        }, 1000);
+        setTimeout(() => {
+            name.classList.remove("name-on")
+            name.classList.remove("animate__fadeOut")
+            name.classList.add("lock", "animate__fadeIn")
+            }, 3000)
+        }, 3000)
 
-    name3.classList.add("name-on")
-    setTimeout(function() {
-        name3.classList.add("move3")
-    }, 3000)
+        setTimeout(() => {
+            name5.classList.add("name-on");
+            setTimeout(() => {
+                name5.classList.add("animate__fadeOut")
+            }, 1000);
+            setTimeout(() => {
+                name5.classList.remove("name-on")
+                name5.classList.remove("animate__fadeOut")
+                name5.classList.add("lock5", "animate__fadeIn")
+                }, 3000)
+            }, 4000)
 
-    name4.classList.add("name-on")
-    setTimeout(function() {
-        name4.classList.add("move4")
-    }, 3000)
-
-    name5.classList.add("name-on")
-    setTimeout(function() {
-        name5.classList.add("move5")
-    }, 3000)
+            setTimeout(() => {
+                name2.classList.add("name-on");
+                setTimeout(() => {
+                    name2.classList.add("animate__fadeOut")
+                }, 1000);
+                setTimeout(() => {
+                    name2.classList.remove("name-on")
+                    name2.classList.remove("animate__fadeOut")
+                    name2.classList.add("lock2", "animate__fadeIn")
+                    }, 3000)
+                }, 5000)
     }, 1000)
+
+
+
+    // setTimeout(() => {
+    //     name.classList.add("name-on");
+    // setTimeout(() => {
+    //     name.classList.add("move")
+    // }, 3000)
+    // name2.classList.add("name-on")
+    // setTimeout(function() {
+    //     name2.classList.add("move2")
+    // }, 3000)
+
+    // name3.classList.add("name-on")
+    // setTimeout(function() {
+    //     name3.classList.add("move3")
+    // }, 3000)
+
+    // name4.classList.add("name-on")
+    // setTimeout(function() {
+    //     name4.classList.add("move4")
+    // }, 3000)
+
+    // name5.classList.add("name-on")
+    // setTimeout(function() {
+    //     name5.classList.add("move5")
+    // }, 3000)
+    // }, 1000)
 
 // container.addEventListener("mouseout", function() {
 //     name.classList.remove("name-on")
